0a4639eeb0a9f1a264651ab0981bd0f9b511c377
Sohhom Bandyopadhyay <sohhom.bandyopadhyay@playpowerlabs.com>
2023-02-01 19:16:24 +0530

    green borders and selectedImageId working

--- experiment.js
+++ experiment.js
@@ -7,30 +7,30 @@ const image_names = [
     './media/Satisfaction.png',
     './media/Shock.png',
     './media/Surprise.png',
-    ]
+]
 
-    const jsPsych = initJsPsych({
-        on_finish: function() {
-        jsPsych.data.displayData();
-    }
-    });
+const jsPsych = initJsPsych({
+    on_finish: function() {
+    jsPsych.data.displayData();
+}
+});
 
-    var timeline = [];
+var timeline = [];
 
-    var trial1 = {
-        type: myplugin2, 
-        image_fnames: [image_names[0], image_names[2], image_names[4], image_names[7]],
-        duration: 800,
-        instruction: "Which image is the best?"
-    }
-    timeline.push(trial1);
-    
-    var trial2 = {
-        type: myplugin2, 
-        image_fnames: [image_names[3], image_names[5], image_names[1], image_names[6]],
-        duration: 800,
-        instruction: "Which image is the best?"
-    }
-    timeline.push(trial2);
+var trial1 = {
+    type: myplugin2, 
+    image_fnames: [image_names[0], image_names[2], image_names[4], image_names[7]],
+    duration: 800,
+    instruction: "Which image is the best?"
+}
+timeline.push(trial1);
 
-    jsPsych.run(timeline)
+var trial2 = {
+    type: myplugin2, 
+    image_fnames: [image_names[3], image_names[5], image_names[1], image_names[6]],
+    duration: 800,
+    instruction: "Which image is the WORST?"
+}
+timeline.push(trial2);
+
+jsPsych.run(timeline)
--- myplugin2.js
+++ myplugin2.js
@@ -32,13 +32,7 @@ var myplugin2 = (function (jspsych) {
 
     trial(display_element, trial)
     {
-     /* todo: create html for 
-     1. the row column layout and the selector mechanism. 
-     2. button with "next" when clicked it ends trial
-     3. implement "Asynchronous loading"
      
-     */
-
       // show image
       let html_content = `
       <div>
@@ -48,31 +42,55 @@ var myplugin2 = (function (jspsych) {
       </div>
       <div class="outer-container">
       <div class="container">
-          <img src="${trial.image_fnames[0]}" class="image">
-          <img src="${trial.image_fnames[1]}" class="image">
-          <img src="${trial.image_fnames[2]}" class="image">
-          <img src="${trial.image_fnames[3]}" class="image">
-      </div>
-      <div class="padding"></div>
+          <img src="${trial.image_fnames[0]}" class="image" id="image01">
+          <img src="${trial.image_fnames[1]}" class="image" id="image02">
+          <img src="${trial.image_fnames[2]}" class="image" id="image03">
+          <img src="${trial.image_fnames[3]}" class="image" id="image04">      
       <div>
-          <button class="next-button">Next</button>
+          <button class="next-button" disabled id="nextButton">Next</button>
       </div>
       </div>
+      </div>
+      <div>
+        <input type="hidden" id="selectedImageId">
       </div>
       `;
+      //console.log(html_content);
       display_element.innerHTML = html_content;
-      console.log(html_content)
-    
-      const after_key_response = (info) => {
-        // hide the image
-        display_element.innerHTML = '';
-    
-        // record the response time as data
+
+      /* 
+      ==================== pasted from ChatGPT ==========
+      */
+      const images = document.querySelectorAll('.image');
+      let selectedImageId;
+
+      images.forEach(image => {
+        image.addEventListener('click', e => {
+          // Remove border from previously selected image
+          if (selectedImageId) {
+            document.getElementById(selectedImageId).style.border = 'none';
+          }
+        // Add border to currently selected image
+          image.style.border = '10px solid #95ae48';
+          selectedImageId = image.id;
+          console.log(selectedImageId);
+          document.getElementById("selectedImageId").value = selectedImageId;
+        });
+      });
+      /*
+      ===========================================
+      */
+
+      const after_key_response = (info) => {  
+        // record the data
         let data = {
-          rt: info.rt
-        }
-    
-        // end the trial
+          rt: info.rt,
+          selectedImageId: document.getElementById("selectedImageId").value,
+        }        
+        console.log(data);
+        // clear the HTML stuff that was previously created
+        display_element.innerHTML = '';    
+        // THIS IS WHERE the trial ENDS
         this.jsPsych.finishTrial(data);
       }
     
